ed3a392a15240a510cdd58242249d430f26a3964
Dillon <dillonroxer@gmail.com>
2019-02-11 12:05:38 -0700

    firstcommit

---
+++ phone.js
@@ -0,0 +1,44 @@
+$(document).ready(function() { // do this when the document is loaded
+	$("#dialer").show(); // show the element with ID "element"
+	$("#add").show();
+	$('#dialer').css('backgroundColor', 'white');
+	$("#contact").show();
+	$("#contacts").hide(); // hide the element with ID "otherElement"
+	$("#addScreen").hide();
+});
+$("#dialer").click(function() {
+
+	$("#numbers").show();
+	$("#addScreen").hide();
+	$("#contacts").hide();
+	$(this).css('backgroundColor', 'white');
+	$('#contact').css('backgroundColor', 'lightgrey');
+	$('#add').css('backgroundColor', 'lightgrey');
+});
+$("#contact").click(function() {
+
+	$("#contacts").show();
+	$("#numbers").hide();
+	$("#addScreen").hide();
+	$(this).css('backgroundColor', 'white');
+	$('#dialer').css('backgroundColor', 'lightgrey');
+	$('#add').css('backgroundColor', 'lightgrey');
+});
+$("#add").click(function() {
+
+	$("#addScreen").show();
+	$("#numbers").hide();
+	$("#contacts").hide();
+	$(this).css('backgroundColor', 'white');
+	$('#contact').css('backgroundColor', 'lightgrey');
+	$('#dialer').css('backgroundColor', 'lightgrey');
+});
+
+/* fancy dialing functions */
+$("#dialer_pad button").click(function() {
+	$("#number_input").val($("#number_input").val() + this.innerText);
+});
+
+$("#button_dialer_clear").click(function() {
+	$("#number_input").val("");
+});
